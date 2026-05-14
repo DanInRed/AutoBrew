b6a8297bf2f1d996bf4557808fdcf5d5f21e41f1
LAB24_USER ON WS09 <L24Y16W09@LabHCI.net>
2026-05-14 15:39:28 +0800

    Updated link

--- app/src/main/java/com/example/autobrew/Login.kt
+++ app/src/main/java/com/example/autobrew/Login.kt
@@ -1,6 +1,9 @@
 package com.example.autobrew
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,20 @@ import androidx.core.view.WindowInsetsCompat
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_root)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        enableEdgeToEdge()
+        val btnLogin = findViewById<Button>(R.id.login_btnLogin)
+        val txtSignup = findViewById<TextView>(R.id.login_tvSignUp)
+
+        btnLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        txtSignup.setOnClickListener{
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/autobrew/Signup.kt
+++ app/src/main/java/com/example/autobrew/Signup.kt
@@ -1,6 +1,9 @@
 package com.example.autobrew
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,17 @@ class Signup : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup_root)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnSignup = findViewById<Button>(R.id.signup_btnSignup)
+        val txtLogin = findViewById<TextView>(R.id.signup_tvLogin)
+
+        btnSignup.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        txtLogin.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
         }
     }
 }
